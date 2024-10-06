af540a6f825215c2bf5aa51788ccf5e9473919cf
Nafisa Binta Tofayel <nafisa1805@cseku.ac.bd>
2024-10-06 18:35:22 +0600

    category added

---
+++ js/video.js
@@ -0,0 +1,24 @@
+console.log('video')
+
+
+// load
+const loadCategories = () =>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then( (res) => res.json())
+        .then( (data) => displayCategories(data.categories))
+        .catch((error) => console.log(error))
+    
+}
+
+// display
+const displayCategories = (categories) =>{
+    const categoryContainer =document.getElementById('categories')
+    categories.forEach( (item) =>{
+        const button = document.createElement('button')
+        button.classList = 'btn'
+        button.innerText = item.category
+        categoryContainer.appendChild(button)
+    })
+}
+
+loadCategories()
